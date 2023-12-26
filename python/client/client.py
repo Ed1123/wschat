8f1cb0ab842957a76cce99b3517addeb4b5c55cb
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-12-26 00:52:22 -0600

    Update client read host and port from args

--- python/client/client.py
+++ python/client/client.py
@@ -1,12 +1,23 @@
 # Echo client program
+import argparse
 import socket
 
-HOST = 'localhost'  # The remote host
-PORT = 50007  # The same port as used by the server
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', type=str, help='host to connect to')
+parser.add_argument('--port', type=int, help='port to connect to')
+args = parser.parse_args()
+
+HOST = args.host or ''
+PORT = args.port or 50007
 # MESSAGE_LENGTH = 1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    try:
+        print(f'Connecting to {HOST}:{PORT}')
+        s.connect((HOST, PORT))
+    except ConnectionRefusedError:
+        print(f'Could not connect to {HOST}:{PORT}')
+        exit(1)
     print(f'Connected to {HOST}:{PORT}')
     print('Press enter to send message to server')
     print('Type "exit" to disconnect from server')
